4547c67c4920cb5749e3ee692633c8a57e9ffcc5
Hai Minh Nguyen <dev.alex.88@gmail.com>
2018-03-12 19:51:55 +0700

    update CircleCI config.yml

--- Scripts/Main Test Cases/TC2_Verify Successful Appointment/Script1482850543053.groovy
+++ Scripts/Main Test Cases/TC2_Verify Successful Appointment/Script1482850543053.groovy
@@ -19,14 +19,13 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 
-
 WebUI.comment('Story: Book an appointment')
 
 WebUI.comment('Given that the user has logged into their account')
 
 WebUI.openBrowser(GlobalVariable.G_SiteURL)
 
-WebUI.callTestCase(findTestCase('Common Test Cases/Login'), ['Username' : 'John Doe', 'Password' : 'ThisIsNotAPassword'], 
+WebUI.callTestCase(findTestCase('Common Test Cases/Login'), [('Username') : 'John Doe', ('Password') : 'ThisIsNotAPassword'], 
     FailureHandling.STOP_ON_FAILURE)
 
 WebUI.comment('And Appointment page is displayed')
@@ -52,17 +51,20 @@ WebUI.verifyTextPresent('Appointment Confirmation', false)
 WebUI.comment('Then he should be able to book a new appointment')
 
 if (true) {
-	WebUI.verifyMatch('Hongkong CURA Healthcare Center', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Facility')), false)
+    WebUI.verifyMatch('Hongkong CURA Healthcare Center', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Facility')), 
+        false)
 
-	WebUI.verifyMatch('Yes', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_HospitalReadmission')), false)
-	
-	WebUI.verifyMatch('Medicaid', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Program')), false)
-	
-	WebUI.verifyMatch('27/12/2016', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_VisitDate')), false)
-	
-	WebUI.verifyMatch('Please make appointment as soon as possible.', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Comment')), false)
-	
+    WebUI.verifyMatch('Yes', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_HospitalReadmission')), false)
+
+    WebUI.verifyMatch('Medicaid', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Program')), false)
+
+    WebUI.verifyMatch('27/12/2016', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_VisitDate')), false)
+
+    WebUI.verifyMatch('Please make appointment as soon as possible.', WebUI.getText(findTestObject('Page_AppointmentConfirmation/lbl_Comment')), 
+        false)
 }
+
+WebUI.takeScreenshot()
 
 WebUI.closeBrowser()
 
